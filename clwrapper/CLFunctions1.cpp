60e9ff525f689c0e01714e4fbc32ff96e7a0fbff
matszpk <matszpk@interia.pl>
2014-07-07 21:33:28 +0000

    CLRadeonExtender: Retain program ntimes only when kernels is not null.

--- clwrapper/CLFunctions1.cpp
+++ clwrapper/CLFunctions1.cpp
@@ -1496,6 +1496,7 @@ clrxclCreateKernelsInProgram(cl_program     program,
             return status;
         
         if (kernels != nullptr)
+        {
             for (kp = 0; kp < kernelsToCreate; kp++)
             {
                 size_t kernelNameSize;
@@ -1535,6 +1536,8 @@ clrxclCreateKernelsInProgram(cl_program     program,
                 outKernel->program = p;
                 kernels[kp] = outKernel;
             }
+            clrxRetainOnlyCLRXProgramNTimes(p, kernelsToCreate);
+        }
     }
     catch(const std::bad_alloc& ex)
     {
@@ -1568,7 +1571,6 @@ clrxclCreateKernelsInProgram(cl_program     program,
     if (num_kernels_ret != nullptr)
         *num_kernels_ret = numKernelsOut;
     
-    clrxRetainOnlyCLRXProgramNTimes(p, kernelsToCreate);
     return CL_SUCCESS;
 }
 
