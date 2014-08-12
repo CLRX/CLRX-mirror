ce2233f9a1c9b6ac7a5e4f7187f1c368d790eae8
matszpk <matszpk@interia.pl>
2014-08-12 22:01:57 +0000

    CLRadeonExtender: Incorrect types in counting maxDigits.

--- utils/NumStringConv.cpp
+++ utils/NumStringConv.cpp
@@ -1225,9 +1225,9 @@ static uint64_t cstrtofXCStyle(const char* str, const char* inend,
             else if (binaryExp >= 0)
                 // if rounding bit is fraction but value have integer part
                 maxDigits = log10ByLog2Ceil(binaryExp) -
-                        (binaryExp-mantisaBits-1); // negative power of rounding bit
+                        (binaryExp-cxint(mantisaBits)-1); // negative power of rounding bit
             else // if all value is fractional value
-                maxDigits = -(binaryExp-mantisaBits-1) -
+                maxDigits = -(binaryExp-cxint(mantisaBits)-1) -
                     log10ByLog2Floor(-binaryExp);
             
             const cxuint maxBigSize = (log10ByLog2Ceil(maxDigits+3)+63)>>6;
