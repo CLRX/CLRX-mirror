b3bc39a88aa8d12df5b0833b2fec0ccab61213d8
matszpk <matszpk@interia.pl>
2014-08-27 22:09:43 +0000

    CLRadeonExtender: Update gcnInstrTable.

--- amdasm/AsmInternals.h
+++ amdasm/AsmInternals.h
@@ -56,13 +56,28 @@ enum : cxuint
 
 enum : cxuint
 {
-    REG_ALL_32 = 0,
-    REG_ALL_64 = 15,
-    REG_DST_64 = 1,
-    REG_SRC0_64 = 2,
-    REG_SRC1_64 = 4,
-    REG_SRC2_64 = 8,
-    REG_DS0_64 = 3,
+    GCN_REG_ALL_32 = 0,
+    GCN_REG_ALL_64 = 15,
+    GCN_REG_DST_64 = 1,
+    GCN_REG_SRC0_64 = 2,
+    GCN_REG_SRC1_64 = 4,
+    GCN_REG_SRC2_64 = 8,
+    GCN_REG_DS0_64 = 3,
+    GCN_MODE_NORMAL = 0,
+    GCN_IMM_NONE = 0x10,
+    GCN_ARG_NONE = 0x20,
+    GCN_REG_S1_JMP = 0x20,
+    GCN_IMM_REL = 0x30,
+    GCN_IMM_LOCKS = 0x40,
+    GCN_IMM_MSGS = 0x50,
+    GCN_SRC2_NONE = 0x60,
+    GCN_SRC2_VCC = 0x70,
+    GCN_SRC12_NONE = 0x80,
+    GCN_MEMOP_MX1 = 0x0,
+    GCN_MEMOP_MX2 = 0x100,
+    GCN_MEMOP_MX4 = 0x200,
+    GCN_MEMOP_MX8 = 0x300,
+    GCN_MEMOP_MX16 = 0x400,
 };
 
 struct GCNInstruction
