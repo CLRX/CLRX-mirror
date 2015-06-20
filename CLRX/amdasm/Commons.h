78b97cee65655b6c9e03a1abd74924c5ac91a63f
matszpk <matszpk@interia.pl>
2015-06-20 11:32:21 +0000

    CLRadeonExtender: missing file.

---
+++ CLRX/amdasm/Commons.h
@@ -0,0 +1,40 @@
+/*
+ *  CLRadeonExtender - Unofficial OpenCL Radeon Extensions Library
+ *  Copyright (C) 2014-2015 Mateusz Szpakowski
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*! \file Commons.h
+ * \brief common definitions for assembler and disassembler
+ */
+
+#ifndef __CLRX_COMMONS_H__
+#define __CLRX_COMMONS_H__
+
+/// main namespace
+namespace CLRX
+{
+
+/// binary for Disassembler
+enum class BinaryFormat
+{
+    AMD = 0,    ///< AMD CATALYST format
+    GALLIUM,     ///< GalliumCompute format
+    RAWCODE     ///< raw code format
+};
+
+};
+
+#endif
