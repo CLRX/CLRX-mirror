10e334b795e00a63c28d0e82bef0bbb2ecedbb5a
matszpk <matszpk@interia.pl>
2016-03-08 18:59:03 +0000

    CLRadeonExtender: Update AmdCL2BinGen.

--- amdbin/AmdCL2BinGen.cpp
+++ amdbin/AmdCL2BinGen.cpp
@@ -18,10 +18,130 @@
  */
 
 #include <CLRX/Config.h>
+#include <cstring>
+#include <cstdint>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <memory>
 #include <CLRX/utils/Containers.h>
 #include <CLRX/utils/InputOutput.h>
 #include <CLRX/amdbin/AmdCL2BinGen.h>
 
 using namespace CLRX;
 
+AmdCL2GPUBinGenerator::AmdCL2GPUBinGenerator() : manageable(false), input(nullptr)
+{ }
 
+AmdCL2GPUBinGenerator::AmdCL2GPUBinGenerator(const AmdCL2Input* amdInput)
+        : manageable(false), input(amdInput)
+{ }
+
+AmdCL2GPUBinGenerator::AmdCL2GPUBinGenerator(GPUDeviceType deviceType,
+       uint32_t driverVersion, size_t globalDataSize, const cxbyte* globalData, 
+       const std::vector<AmdCL2KernelInput>& kernelInputs)
+        : manageable(true), input(nullptr)
+{
+    input = new AmdCL2Input{deviceType, globalDataSize, globalData,
+                driverVersion, "", "", kernelInputs };
+}
+
+AmdCL2GPUBinGenerator::AmdCL2GPUBinGenerator(GPUDeviceType deviceType,
+       uint32_t driverVersion, size_t globalDataSize, const cxbyte* globalData, 
+       std::vector<AmdCL2KernelInput>&& kernelInputs)
+        : manageable(true), input(nullptr)
+{
+    input = new AmdCL2Input{deviceType, globalDataSize, globalData,
+                driverVersion, "", "", std::move(kernelInputs) };
+}
+
+AmdCL2GPUBinGenerator::~AmdCL2GPUBinGenerator()
+{
+    if (manageable)
+        delete input;
+}
+
+void AmdCL2GPUBinGenerator::setInput(const AmdCL2Input* input)
+{
+    if (manageable)
+        delete input;
+    manageable = false;
+    this->input = input;
+}
+
+static const uint32_t gpuDeviceCodeTable[16] =
+{
+    0, // GPUDeviceType::CAPE_VERDE
+    0, // GPUDeviceType::PITCAIRN
+    0, // GPUDeviceType::TAHITI
+    0, // GPUDeviceType::OLAND
+    6, // GPUDeviceType::BONAIRE
+    1, // GPUDeviceType::SPECTRE
+    2, // GPUDeviceType::SPOOKY
+    3, // GPUDeviceType::KALINDI
+    0, // GPUDeviceType::HAINAN
+    7, // GPUDeviceType::HAWAII
+    8, // GPUDeviceType::ICELAND
+    9, // GPUDeviceType::TONGA
+    4, // GPUDeviceType::MULLINS
+    17, // GPUDeviceType::FIJI
+    16, // GPUDeviceType::CARRIZO
+    15 // GPUDeviceType::DUMMY
+};
+
+// fast and memory efficient String table generator for main binary
+class CLRX_INTERNAL MainStrTabGen: public ElfRegionContent
+{
+private:
+    const AmdCL2Input* input;
+public:
+    MainStrTabGen(const AmdCL2Input* _input) : input(_input)
+    { }
+    
+    size_t size() const
+    {
+        return 0;
+    }
+    
+    void operator()(FastOutputBuffer& fob) const
+    {
+    }
+};
+
+
+/// main routine to generate OpenCL 2.0 binary
+void AmdCL2GPUBinGenerator::generateInternal(std::ostream* osPtr, std::vector<char>* vPtr,
+             Array<cxbyte>* aPtr) const
+{
+    const size_t kernelsNum = input->kernels.size();
+    const bool oldBinaries = input->driverVersion < 191205;
+    const GPUArchitecture arch = getGPUArchitectureFromDeviceType(input->deviceType);
+    if (arch == GPUArchitecture::GCN1_0)
+        throw Exception("OpenCL 2.0 supported only for GCN1.1 or later");
+    
+    ElfBinaryGen64 elfBinGen({ 0, 0, ELFOSABI_SYSV, 0, ET_EXEC,  0xaf5b, EV_CURRENT,
+                UINT_MAX, 0, gpuDeviceCodeTable[cxuint(input->deviceType)] });
+    {
+        elfBinGen.addRegion(ElfRegion64(0, (const cxbyte*)nullptr, 1, ".shstrtab",
+                                SHT_STRTAB, SHF_STRINGS));
+        MainStrTabGen mainStrTabGen(input);
+        elfBinGen.addRegion(ElfRegion64(mainStrTabGen.size(), &mainStrTabGen, 1,
+                     ".strtab", SHT_STRTAB, SHF_STRINGS));
+    }
+}
+
+void AmdCL2GPUBinGenerator::generate(Array<cxbyte>& array) const
+{
+    generateInternal(nullptr, nullptr, &array);
+}
+
+void AmdCL2GPUBinGenerator::generate(std::ostream& os) const
+{
+    generateInternal(&os, nullptr, nullptr);
+}
+
+void AmdCL2GPUBinGenerator::generate(std::vector<char>& vector) const
+{
+    generateInternal(nullptr, &vector, nullptr);
+}
