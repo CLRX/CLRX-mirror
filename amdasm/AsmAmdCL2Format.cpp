2ba6508353f8bf6efa6e6359d0f10f33fe340541
matszpk <matszpk@interia.pl>
2016-04-09 18:53:55 +0000

    CLRadeonExtender: AsmAmdCL2: Small fix for resolving relocations.

--- amdasm/AsmAmdCL2Format.cpp
+++ amdasm/AsmAmdCL2Format.cpp
@@ -1503,12 +1503,12 @@ bool AsmAmdCL2Handler::resolveRelocation(const AsmExpression* expr, uint64_t& ou
             case AsmExprOp::MODULO:
             case AsmExprOp::SIGNED_MODULO:
                 relType = RELTYPE_LOW_32BIT;
-                good = ((secondArg & 0xffffffffULL) == 0);
+                good = ((secondArg>>32)!=0 && (secondArg & 0xffffffffULL) == 0);
                 break;
             case AsmExprOp::DIVISION:
             case AsmExprOp::SIGNED_DIVISION:
                 relType = RELTYPE_HIGH_32BIT;
-                good = ((secondArg & 0xffffffffULL) == 0);
+                good = ((secondArg>>32)!=0 && (secondArg & 0xffffffffULL) == 0);
                 break;
             case AsmExprOp::SHIFT_RIGHT:
                 relType = RELTYPE_HIGH_32BIT;
