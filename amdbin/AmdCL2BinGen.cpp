6f74980b8d79837ce80437079272e124ab159837
matszpk <matszpk@interia.pl>
2016-04-23 07:41:11 +0000

    CLRadeonExtender: AmdCL2BinGen: Check relocations only for new binaries.

--- amdbin/AmdCL2BinGen.cpp
+++ amdbin/AmdCL2BinGen.cpp
@@ -286,13 +286,14 @@ static void prepareKernelTempData(const AmdCL2Input* input,
         if (newBinaries && (kernel.stubSize!=0 || kernel.stub!=nullptr))
             throw Exception("Kernel stub allowed for old driver binaries");
         /* check relocations */
-        for (const AmdCL2RelInput& rel: kernel.relocations)
-        {
-            if (rel.type > RELTYPE_HIGH_32BIT || rel.symbol > 2)
-                throw Exception("Wrong relocation symbol or type");
-            if (rel.offset+4 > kernel.codeSize)
-                throw Exception("Relocation offset outside code size");
-        }
+        if (newBinaries)
+            for (const AmdCL2RelInput& rel: kernel.relocations)
+            {
+                if (rel.type > RELTYPE_HIGH_32BIT || rel.symbol > 2)
+                    throw Exception("Wrong relocation symbol or type");
+                if (rel.offset+4 > kernel.codeSize)
+                    throw Exception("Relocation offset outside code size");
+            }
         
         if (!kernel.useConfig)
         {   // if no kernel configuration
