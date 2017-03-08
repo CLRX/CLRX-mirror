1839e6168b40ac2c008545e994a6396bbb4eb542
matszpk <matszpk@interia.pl>
2017-03-08 14:40:22 +0000

    CLRadeonExtender: Move regalloc code (ISAUsageHandler) to AsmRegAlloc.cpp source.

---
+++ amdasm/AsmRegAlloc.cpp
@@ -0,0 +1,231 @@
+/*
+ *  CLRadeonExtender - Unofficial OpenCL Radeon Extensions Library
+ *  Copyright (C) 2014-2017 Mateusz Szpakowski
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <CLRX/Config.h>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <CLRX/utils/Utilities.h>
+#include <CLRX/amdasm/Assembler.h>
+#include "AsmInternals.h"
+
+using namespace CLRX;
+
+ISAUsageHandler::ISAUsageHandler(const std::vector<cxbyte>& _content) :
+            content(_content), lastOffset(0), readOffset(0), instrStructPos(0),
+            regUsagesPos(0), regUsages2Pos(0), regVarUsagesPos(0),
+            pushedArgs(0), argPos(0), argFlags(0), isNext(false), useRegMode(false)
+{ }
+
+ISAUsageHandler::~ISAUsageHandler()
+{ }
+
+void ISAUsageHandler::rewind()
+{
+    readOffset = instrStructPos = 0;
+    regUsagesPos = regUsages2Pos = regVarUsagesPos = 0;
+    useRegMode = false;
+    pushedArgs = 0;
+    skipBytesInInstrStruct();
+}
+
+void ISAUsageHandler::skipBytesInInstrStruct()
+{
+    // do not add instruction size if usereg (usereg immediately before instr regusages)
+    if ((instrStructPos != 0 || argPos != 0) && !useRegMode)
+        readOffset += defaultInstrSize;
+    argPos = 0;
+    for (;instrStructPos < instrStruct.size() &&
+        instrStruct[instrStructPos] > 0x80; instrStructPos++)
+        readOffset += (instrStruct[instrStructPos] & 0x7f);
+    isNext = (instrStructPos < instrStruct.size());
+}
+
+void ISAUsageHandler::putSpace(size_t offset)
+{
+    if (lastOffset != offset)
+    {
+        flush(); // flush before new instruction
+        // useReg immediately before instruction regusages
+        size_t defaultInstrSize = (!useRegMode ? this->defaultInstrSize : 0);
+        if (lastOffset > offset)
+            throw Exception("Offset before previous instruction");
+        if (!instrStruct.empty() && offset - lastOffset < defaultInstrSize)
+            throw Exception("Offset between previous instruction");
+        size_t toSkip = !instrStruct.empty() ? 
+                offset - lastOffset - defaultInstrSize : offset;
+        while (toSkip > 0)
+        {
+            size_t skipped = std::min(toSkip, size_t(0x7f));
+            instrStruct.push_back(skipped | 0x80);
+            toSkip -= skipped;
+        }
+        lastOffset = offset;
+        argFlags = 0;
+        pushedArgs = 0;
+    } 
+}
+
+void ISAUsageHandler::pushUsage(const AsmRegVarUsage& rvu)
+{
+    if (lastOffset == rvu.offset && useRegMode)
+        flush(); // only flush if useRegMode and no change in offset
+    else // otherwise
+        putSpace(rvu.offset);
+    useRegMode = false;
+    if (rvu.regVar != nullptr)
+    {
+        argFlags |= (1U<<pushedArgs);
+        regVarUsages.push_back({ rvu.regVar, rvu.rstart, rvu.rend, rvu.regField,
+            rvu.rwFlags, rvu.align });
+    }
+    else // reg usages
+        regUsages.push_back({ rvu.regField,cxbyte(rvu.rwFlags |
+                    getRwFlags(rvu.regField, rvu.rstart, rvu.rend)) });
+    pushedArgs++;
+}
+
+void ISAUsageHandler::pushUseRegUsage(const AsmRegVarUsage& rvu)
+{
+    if (lastOffset == rvu.offset && !useRegMode)
+        flush(); // only flush if useRegMode and no change in offset
+    else // otherwise
+        putSpace(rvu.offset);
+    useRegMode = true;
+    if (pushedArgs == 0 || pushedArgs == 256)
+    {
+        argFlags = 0;
+        pushedArgs = 0;
+        instrStruct.push_back(0x80); // sign of regvarusage from usereg
+        instrStruct.push_back(0);
+    }
+    if (rvu.regVar != nullptr)
+    {
+        argFlags |= (1U<<(pushedArgs & 7));
+        regVarUsages.push_back({ rvu.regVar, rvu.rstart, rvu.rend, rvu.regField,
+            rvu.rwFlags, rvu.align });
+    }
+    else // reg usages
+        regUsages2.push_back({ rvu.rstart, rvu.rend, rvu.rwFlags });
+    pushedArgs++;
+    if ((pushedArgs & 7) == 0) // just flush per 8 bit
+    {
+        instrStruct.push_back(argFlags);
+        instrStruct[instrStruct.size() - ((pushedArgs+7) >> 3) - 1] = pushedArgs;
+        argFlags = 0;
+    }
+}
+
+void ISAUsageHandler::flush()
+{
+    if (pushedArgs != 0)
+    {
+        if (!useRegMode)
+        {   // normal regvarusages
+            instrStruct.push_back(argFlags);
+            if ((argFlags & (1U<<(pushedArgs-1))) != 0)
+                regVarUsages.back().rwFlags |= 0x80;
+            else // reg usages
+                regUsages.back().rwFlags |= 0x80;
+        }
+        else // use reg regvarusages
+        {
+            if ((pushedArgs & 7) != 0) //if only not pushed args remains
+                instrStruct.push_back(argFlags);
+            instrStruct[instrStruct.size() - ((pushedArgs+7) >> 3) - 1] = pushedArgs;
+        }
+    }
+}
+
+AsmRegVarUsage ISAUsageHandler::nextUsage()
+{
+    if (!isNext)
+        throw Exception("No reg usage in this code");
+    AsmRegVarUsage rvu;
+    // get regvarusage
+    bool lastRegUsage = false;
+    rvu.offset = readOffset;
+    if (!useRegMode && instrStruct[instrStructPos] == 0x80)
+    {   // useRegMode (begin fetching useregs)
+        useRegMode = true;
+        argPos = 0;
+        instrStructPos++;
+        // pushedArgs - numer of useregs, 0 - 256 useregs
+        pushedArgs = instrStruct[instrStructPos++];
+        argFlags = instrStruct[instrStructPos];
+    }
+    
+    if ((instrStruct[instrStructPos] & (1U << (argPos&7))) != 0)
+    {   // regvar usage
+        const AsmRegVarUsageInt& inRVU = regVarUsages[regVarUsagesPos++];
+        rvu.regVar = inRVU.regVar;
+        rvu.rstart = inRVU.rstart;
+        rvu.rend = inRVU.rend;
+        rvu.regField = inRVU.regField;
+        rvu.rwFlags = inRVU.rwFlags & ASMRVU_ACCESS_MASK;
+        rvu.align = inRVU.align;
+        if (!useRegMode)
+            lastRegUsage = ((inRVU.rwFlags&0x80) != 0);
+    }
+    else if (!useRegMode)
+    {   // simple reg usage
+        const AsmRegUsageInt& inRU = regUsages[regUsagesPos++];
+        rvu.regVar = nullptr;
+        const std::pair<uint16_t, uint16_t> regPair =
+                    getRegPair(inRU.regField, inRU.rwFlags);
+        rvu.rstart = regPair.first;
+        rvu.rend = regPair.second;
+        rvu.rwFlags = (inRU.rwFlags & ASMRVU_ACCESS_MASK);
+        rvu.regField = inRU.regField;
+        rvu.align = 0;
+        lastRegUsage = ((inRU.rwFlags&0x80) != 0);
+    }
+    else
+    {   // use reg (simple reg usage, second structure)
+        const AsmRegUsage2Int& inRU = regUsages2[regUsages2Pos++];
+        rvu.regVar = nullptr;
+        rvu.rstart = inRU.rstart;
+        rvu.rend = inRU.rend;
+        rvu.rwFlags = inRU.rwFlags;
+        rvu.regField = ASMFIELD_NONE;
+        rvu.align = 0;
+    }
+    argPos++;
+    if (useRegMode)
+    {   // if inside useregs
+        if (argPos == (pushedArgs&0xff))
+        {
+            instrStructPos++; // end
+            skipBytesInInstrStruct();
+            useRegMode = false;
+        }
+        else if ((argPos & 7) == 0) // fetch new flag
+        {
+            instrStructPos++;
+            argFlags = instrStruct[instrStructPos];
+        }
+    }
+    // after instr
+    if (lastRegUsage)
+    {
+        instrStructPos++;
+        skipBytesInInstrStruct();
+    }
+    return rvu;
+}
