83b32c947a3bd7921d2fa63d0cb75c46016a617a
matszpk <matszpk@interia.pl>
2017-07-23 12:40:05 +0000

    CLRadeonExtender: GCNAsm: Fixed yet another possible out of line checking.

--- amdasm/GCNAsmHelpers.cpp
+++ amdasm/GCNAsmHelpers.cpp
@@ -94,7 +94,7 @@ bool GCNAsmUtils::parseRegVarRange(Assembler& asmr, const char*& linePtr,
             ((flags & INSTROP_SREGS)!=0 && regVar->type==REGTYPE_SGPR))
         {
             skipSpacesToEnd(linePtr, end);
-            if (*linePtr == '[')
+            if (linePtr != end && *linePtr == '[')
             {
                 uint64_t value1, value2;
                 skipCharAndSpacesToEnd(linePtr, end);
