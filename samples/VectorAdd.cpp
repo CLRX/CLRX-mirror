70b77c0eda0e975369ba8e016e66e7f2f5a472bc
matszpk <matszpk@interia.pl>
2017-11-30 13:47:31 +0000

    CLRadeonExtender: Update VectorAdd sample for GFX900 (VEGA).

--- samples/VectorAdd.cpp
+++ samples/VectorAdd.cpp
@@ -27,9 +27,57 @@ using namespace CLRX;
 
 static const char* vectorAddSource = R"ffDXD(# VectorAdd example
 SMUL = 1
+GCN1_2_4 = 0
 .ifarch GCN1.2
+    GCN1_2_4 = 1
+    SMUL = 4
+.elseifarch GCN1.4
+    GCN1_2_4 = 1
     SMUL = 4
 .endif
+
+.ifarch GCN1.4
+    # helper macros for integer add/sub instructions
+    .macro VADD_U32 dest, cdest, src0, src1, mods:vararg
+        v_add_co_u32 \dest, \cdest, \src0, \src1 \mods
+    .endm
+    .macro VADDC_U32 dest, cdest, src0, src1, csrc, mods:vararg
+        v_addc_co_u32 \dest, \cdest, \src0, \src1, \csrc \mods
+    .endm
+    .macro VSUB_U32 dest, cdest, src0, src1, mods:vararg
+        v_sub_co_u32 \dest, \cdest, \src0, \src1 \mods
+    .endm
+    .macro VSUBB_U32 dest, cdest, src0, src1, csrc, mods:vararg
+        v_subb_co_u32 \dest, \cdest, \src0, \src1, \csrc \mods
+    .endm
+    .macro VSUBREV_U32 dest, cdest, src0, src1, mods:vararg
+        v_subrev_co_u32 \dest, \cdest, \src0, \src1 \mods
+    .endm
+    .macro VSUBBREV_U32 dest, cdest, src0, src1, csrc, mods:vararg
+        v_subbrev_co_u32 \dest, \cdest, \src0, \src1, \csrc \mods
+    .endm
+.else
+    # helper macros for integer add/sub instructions
+    .macro VADD_U32 dest, cdest, src0, src1, mods:vararg
+        v_add_u32 \dest, \cdest, \src0, \src1 \mods
+    .endm
+    .macro VADDC_U32 dest, cdest, src0, src1, csrc, mods:vararg
+        v_addc_u32 \dest, \cdest, \src0, \src1, \csrc \mods
+    .endm
+    .macro VSUB_U32 dest, cdest, src0, src1, mods:vararg
+        v_sub_u32 \dest, \cdest, \src0, \src1 \mods
+    .endm
+    .macro VSUBB_U32 dest, cdest, src0, src1, csrc, mods:vararg
+        v_subb_u32 \dest, \cdest, \src0, \src1, \csrc \mods
+    .endm
+    .macro VSUBREV_U32 dest, cdest, src0, src1, mods:vararg
+        v_subrev_u32 \dest, \cdest, \src0, \src1 \mods
+    .endm
+    .macro VSUBBREV_U32 dest, cdest, src0, src1, csrc, mods:vararg
+        v_subbrev_u32 \dest, \cdest, \src0, \src1, \csrc \mods
+    .endm
+.endif
+
 .iffmt amd    # if AMD Catalyst
 .kernel vectorAdd
     .config
@@ -46,7 +94,7 @@ SMUL = 1
         .userdata imm_const_buffer, 1, 8, 4     # s[8:11] - put kernel argument const buffer
     .text
     GCN12And64Bit = 0
-    .ifarch GCN1.2
+    .if GCN1_2_4
         .if64
             GCN12And64Bit = 1
         .endif
@@ -70,7 +118,7 @@ SMUL = 1
         s_waitcnt lgkmcnt(0)                    # wait for results
         s_mul_i32 s0, s0, s12            # s0 - local_size(0)*group_id(0) (s12)
         s_add_u32 s0, s0, s1             # s0 - local_size(0)*group_id(0)+global_offset(0)
-        v_add_i32 v0, vcc, s0, v0        # v0 - s0+local_id(0) -> global_id(0)
+        VADD_U32 v0, vcc, s0, v0        # v0 - s0+local_id(0) -> global_id(0)
         v_cmp_gt_u32 vcc, s4, v0         # global_id(0) < n
         s_and_saveexec_b64 s[0:1], vcc          # lock all threads with id>=n
         s_cbranch_execz end                     # no active threads, we jump to end
@@ -85,12 +133,12 @@ SMUL = 1
     .else # 64bit
         v_lshrrev_b32 v1, 30, v0
         v_lshlrev_b32 v0, 2, v0                 # v[0:1] - global_id(0)*4
-        v_add_i32 v2,vcc,s16,v0                 # v[2:3] - abuf offset + global_id(0)*2
+        VADD_U32 v2,vcc,s16,v0                 # v[2:3] - abuf offset + global_id(0)*2
         v_mov_b32 v3, s17                       # move to vector reg
-        v_addc_u32 v3,vcc,v3,v1,vcc             # v_addc_u32 with only vector regs
-        v_add_i32 v4,vcc,s18,v0                 # v[4:5] - bbuf offset + global_id(0)*2
+        VADDC_U32 v3,vcc,v3,v1,vcc             # VADDC_U32 with only vector regs
+        VADD_U32 v4,vcc,s18,v0                 # v[4:5] - bbuf offset + global_id(0)*2
         v_mov_b32 v5, s19                       # move to vector reg
-        v_addc_u32 v5,vcc,v5,v1,vcc             # v_addc_u32 with only vector regs
+        VADDC_U32 v5,vcc,v5,v1,vcc             # VADDC_U32 with only vector regs
         s_waitcnt lgkmcnt(0)                    # wait for results
         buffer_load_dword v2, v[2:3], s[8:11], 0 addr64     # value from aBuf
         buffer_load_dword v3, v[4:5], s[12:15], 0 addr64    # value from bBuf
@@ -103,9 +151,9 @@ SMUL = 1
         buffer_store_dword v1, v0, s[8:11], s7 offen       # write value to cBuf
     .else # 64bit
         v_add_f32 v2, v2, v3        # add two values
-        v_add_i32 v0,vcc,s6,v0                  # v[0:1] - cbuf offset + global_id(0)*2
+        VADD_U32 v0,vcc,s6,v0                  # v[0:1] - cbuf offset + global_id(0)*2
         v_mov_b32 v3, s7                        # move to vector reg
-        v_addc_u32 v1,vcc,v3,v1,vcc             # v_addc_u32 with only vector regs
+        VADDC_U32 v1,vcc,v3,v1,vcc             # VADDC_U32 with only vector regs
         s_waitcnt lgkmcnt(0)         # wait for cBuf descriptor and offset
         buffer_store_dword v2, v[0:1], s[8:11], 0 addr64    # value to cBuf
     .endif
@@ -121,7 +169,7 @@ SMUL = 1
         
         s_mul_i32 s0, s0, s12            # s0 - local_size(0)*group_id(0) (s12)
         s_add_u32 s0, s0, s1             # s0 - local_size(0)*group_id(0)+global_offset(0)
-        v_add_i32 v0, vcc, s0, v0        # v0 - s0+local_id(0) -> global_id(0)
+        VADD_U32 v0, vcc, s0, v0        # v0 - s0+local_id(0) -> global_id(0)
         v_cmp_gt_u32 vcc, s4, v0         # global_id(0) < n
         s_and_saveexec_b64 s[0:1], vcc          # lock all threads with id>=n
         s_cbranch_execz end                     # no active threads, we jump to end
@@ -135,13 +183,13 @@ SMUL = 1
         s_add_u32 s8, s8, s16       # s[8:9] - aBuffer base + aBuffer offset
         s_addc_u32 s9, s9, s17
         v_mov_b32 v3, s9            # v[2:3] - s[8:9] + global_id(0)*4
-        v_add_u32 v2, vcc, s8, v0
-        v_addc_u32 v3, vcc, v3, v1, vcc
+        VADD_U32 v2, vcc, s8, v0
+        VADDC_U32 v3, vcc, v3, v1, vcc
         s_add_u32 s12, s12, s18       # s[12:13] - bBuffer base + bBuffer offset
         s_addc_u32 s13, s13, s19
         v_mov_b32 v5, s13            # v[4:5] - s[12:13] + global_id(0)*4
-        v_add_u32 v4, vcc, s12, v0
-        v_addc_u32 v5, vcc, v5, v1, vcc
+        VADD_U32 v4, vcc, s12, v0
+        VADDC_U32 v5, vcc, v5, v1, vcc
         flat_load_dword v6, v[2:3]      # load a[i]
         flat_load_dword v7, v[4:5]      # load b[i]
         s_waitcnt vmcnt(0)                      # wait for data
@@ -153,13 +201,14 @@ SMUL = 1
         s_add_u32 s8, s8, s6         # s[12:13] - cBuffer base + cBuffer offset
         s_addc_u32 s9, s9, s7
         v_mov_b32 v3, s9             # v[4:5] - s[12:13] + global_id(0)*4
-        v_add_u32 v2, vcc, s8, v0
-        v_addc_u32 v3, vcc, v3, v1, vcc
+        VADD_U32 v2, vcc, s8, v0
+        VADDC_U32 v3, vcc, v3, v1, vcc
         flat_store_dword v[2:3], v6
     .endif
 end:
         s_endpgm
-.elseiffmt amdcl2  # AMD OpenCL 2.0 code
+)ffDXD"  // MSVC fix: string too big
+R"ffDXD(.elseiffmt amdcl2  # AMD OpenCL 2.0 code
 .kernel vectorAdd
     .config
         .dims x
@@ -171,6 +220,11 @@ end:
         .arg bBuf, float*, global, const    # argument const float* bBuf
         .arg cBuf, float*, global           # argument float* cBuf
     .text
+    .ifarch GCN1.4
+        GID = %s10
+    .else
+        GID = %s8
+    .endif
     .if32 # 32-bit
         s_load_dwordx2 s[0:1], s[6:7], 7*SMUL   # get aBuf and bBuf pointers
         s_load_dword s4, s[4:5], 1*SMUL         # get local info dword
@@ -178,9 +232,9 @@ end:
         s_load_dword s3, s[6:7], 6*SMUL         # get n - number of elems
         s_waitcnt lgkmcnt(0)                    # wait for data
         s_and_b32 s4, s4, 0xffff            # only first localsize(0)
-        s_mul_i32 s4, s8, s4                # localsize*groupId
+        s_mul_i32 s4, GID, s4                # localsize*groupId
         s_add_u32 s4, s2, s4                # localsize*groupId+offset
-        v_add_i32 v0, vcc, s4, v0           # final global_id
+        VADD_U32 v0, vcc, s4, v0           # final global_id
         v_cmp_gt_u32 vcc, s3, v0            # global_id(0) < n
         s_and_saveexec_b64 s[4:5], vcc          # lock all threads with id>=n
         s_cbranch_execz end                     # no active threads, we jump to end
@@ -191,15 +245,15 @@ end:
         s_mov_b32 s11, 0x8027fac                # set dstsel, nfmt and dfmt
         
         v_lshlrev_b32 v0, 2, v0                 # v0 - global_id(0)*4
-        v_add_i32 v1, vcc, s0, v0               # aBuf+get_global_id(0)
-        v_add_i32 v2, vcc, s1, v0               # bBuf+get_global_id(0)
+        VADD_U32 v1, vcc, s0, v0               # aBuf+get_global_id(0)
+        VADD_U32 v2, vcc, s1, v0               # bBuf+get_global_id(0)
         buffer_load_dword v1, v1, s[8:11], 0 offen # load value from aBuf
         buffer_load_dword v2, v2, s[8:11], 0 offen # load value from bBuf
         s_waitcnt vmcnt(0)                      # wait for data
         v_add_f32 v1, v1, v2                    # add values
         s_load_dword s0, s[6:7], 9*SMUL         # get cBuf pointer
         s_waitcnt lgkmcnt(0)                    # wait for data
-        v_add_i32 v2, vcc, s0, v0               # cBuf+get_global_id(0)
+        VADD_U32 v2, vcc, s0, v0               # cBuf+get_global_id(0)
         buffer_store_dword v1, v2, s[8:11], 0 offen # store value to cBuf
         
     .else # 64-bit
@@ -209,21 +263,21 @@ end:
         s_load_dword s5, s[6:7], 12*SMUL        # get n - number of elems
         s_waitcnt lgkmcnt(0)                    # wait for data
         s_and_b32 s4, s4, 0xffff            # only first localsize(0)
-        s_mul_i32 s4, s8, s4                # localsize*groupId
+        s_mul_i32 s4, GID, s4                # localsize*groupId
         s_add_u32 s4, s9, s4                # localsize*groupId+offset
-        v_add_i32 v0, vcc, s4, v0           # final global_id
+        VADD_U32 v0, vcc, s4, v0           # final global_id
         v_cmp_gt_u32 vcc, s5, v0            # global_id(0) < n
         s_and_saveexec_b64 s[4:5], vcc          # lock all threads with id>=n
         s_cbranch_execz end                     # no active threads, we jump to end
         
         v_lshrrev_b32 v1, 30, v0
         v_lshlrev_b32 v0, 2, v0             # v[0:1] - global_id(0)*4
-        v_add_i32 v2, vcc, s0, v0           # aBuf+get_global_id(0)
+        VADD_U32 v2, vcc, s0, v0           # aBuf+get_global_id(0)
         v_mov_b32 v3, s1
-        v_addc_u32 v3, vcc, 0, v3, vcc      # aBuf+get_global_id(0) - higher part
-        v_add_i32 v4, vcc, s2, v0           # bBuf+get_global_id(0)
+        VADDC_U32 v3, vcc, 0, v3, vcc      # aBuf+get_global_id(0) - higher part
+        VADD_U32 v4, vcc, s2, v0           # bBuf+get_global_id(0)
         v_mov_b32 v5, s3
-        v_addc_u32 v5, vcc, 0, v5, vcc      # bBuf+get_global_id(0) - higher part
+        VADDC_U32 v5, vcc, 0, v5, vcc      # bBuf+get_global_id(0) - higher part
         flat_load_dword v2, v[2:3]          # load value from aBuf
         flat_load_dword v4, v[4:5]          # load value from bBuf
         s_waitcnt vmcnt(0) & lgkmcnt(0)     # wait for data
@@ -231,9 +285,9 @@ end:
         
         s_load_dwordx2 s[0:1], s[6:7], 18*SMUL  # get cBuf pointer
         s_waitcnt lgkmcnt(0)
-        v_add_i32 v0, vcc, s0, v0           # cBuf+get_global_id(0)
+        VADD_U32 v0, vcc, s0, v0           # cBuf+get_global_id(0)
         v_mov_b32 v3, s1
-        v_addc_u32 v1, vcc, 0, v3, vcc      # cBuf+get_global_id(0) - higher part
+        VADDC_U32 v1, vcc, 0, v3, vcc      # cBuf+get_global_id(0) - higher part
         flat_store_dword v[0:1], v2         # store value to cBuf
     .endif
 end:
@@ -272,12 +326,12 @@ vectorAdd:
         s_and_b32 s0, s0, 0xffff           # only local size X
         s_mul_i32 s0, s0, s8               # s0 - localSize*groupId
         s_add_i32 s0, s0, s1               # s0 - localSize*groupId + offset
-        v_add_u32 v0, vcc, s0, v0          # v0 - localId + localSize*groupId + offset
+        VADD_U32 v0, vcc, s0, v0          # v0 - localId + localSize*groupId + offset
         v_cmp_gt_u32 vcc, s2, v0           # compare n > id
         s_and_saveexec_b64 s[0:1], vcc
         s_cbranch_execz end             # end
         v_mov_b32 v1, 0
-    .ifnarch GCN1.2
+    .ifeq GCN1_2_4  # GCN 1.0/1.1
         s_load_dwordx2  s[0:1], s[6:7], 2*SMUL      # prepare Aptr buffer
         s_mov_b32       s2, 0
         s_mov_b32       s3, 0xf000
@@ -292,23 +346,23 @@ vectorAdd:
         s_waitcnt vmcnt(0)
         v_add_f32 v2, v3, v2
         buffer_store_dword v2, v[0:1], s[12:15], 0 addr64
-    .else
+    .else   # GCN 1.2
         s_load_dwordx4  s[0:3], s[6:7], 2*SMUL      # load Aptr and Bptr
         s_load_dwordx2  s[4:5], s[6:7], 6*SMUL      # load Cptr
         s_waitcnt lgkmcnt(0)
         v_lshlrev_b64 v[0:1], 2, v[0:1]     # make vector offset
-        v_add_u32 v2, vcc, s0, v0           # Aptr + offset
+        VADD_U32 v2, vcc, s0, v0           # Aptr + offset
         v_mov_b32 v3, s1
-        v_addc_u32 v3, vcc, v3, v1, vcc
-        v_add_u32 v4, vcc, s2, v0           # Bptr + offset
+        VADDC_U32 v3, vcc, v3, v1, vcc
+        VADD_U32 v4, vcc, s2, v0           # Bptr + offset
         v_mov_b32 v5, s3
-        v_addc_u32 v5, vcc, v5, v1, vcc
+        VADDC_U32 v5, vcc, v5, v1, vcc
         flat_load_dword v6, v[2:3]          # load A value
         flat_load_dword v7, v[4:5]          # load B value
         s_waitcnt vmcnt(0)          # wait for values
-        v_add_u32 v0, vcc, s4, v0           # Cptr + offset
+        VADD_U32 v0, vcc, s4, v0           # Cptr + offset
         v_mov_b32 v2, s5
-        v_addc_u32 v1, vcc, v2, v1, vcc
+        VADDC_U32 v1, vcc, v2, v1, vcc
         v_add_f32 v6, v7, v6            # add values
         flat_store_dword v[0:1], v6
     .endif
@@ -322,11 +376,11 @@ vectorAdd:
         s_waitcnt lgkmcnt(0)            # wait for results
         s_mul_i32 s0, s2, s4            # s0 - local_size(0)*group_id(0)
         s_add_u32 s0, s0, s1            # s0 - local_size(0)*group_id(0)+global_offset(0)
-        v_add_i32 v0, vcc, s0, v0       # v0 - s0+local_id(0) -> global_id(0)
+        VADD_U32 v0, vcc, s0, v0       # v0 - s0+local_id(0) -> global_id(0)
         v_cmp_gt_u32 vcc, s3, v0                # global_id(0) < n
         s_and_saveexec_b64 s[0:1], vcc          # lock all threads with id>=n
         s_cbranch_execz end                     # no active threads, we jump to end
-    .ifnarch GCN1.2
+    .ifeq GCN1_2_4    # GCN 1.0/1.1
         # generate buffer descriptors
         s_mov_b32 s4, s6            # cbuffer - s[4:7]
         s_mov_b32 s5, s7
@@ -347,18 +401,18 @@ vectorAdd:
         v_lshrrev_b32 v1, 30, v0    # v[0:1] - global_id(0)*2
         v_lshlrev_b32 v0, 2, v0
         v_mov_b32 v3, s9
-        v_add_u32 v2, vcc, s8, v0           # add gid*4 to aBuffer pointer
-        v_addc_u32 v3, vcc, v3, v1, vcc
+        VADD_U32 v2, vcc, s8, v0           # add gid*4 to aBuffer pointer
+        VADDC_U32 v3, vcc, v3, v1, vcc
         v_mov_b32 v5, s13
-        v_add_u32 v4, vcc, s12, v0           # add gid*4 to bBuffer pointer
-        v_addc_u32 v5, vcc, v5, v1, vcc
+        VADD_U32 v4, vcc, s12, v0           # add gid*4 to bBuffer pointer
+        VADDC_U32 v5, vcc, v5, v1, vcc
         flat_load_dword v6, v[2:3]          # load value from aBuffer
         flat_load_dword v7, v[4:5]          # load value from bBuffer
         s_waitcnt vmcnt(0)
         v_add_f32 v6, v6, v7                # add values
         v_mov_b32 v3, s7
-        v_add_u32 v2, vcc, s6, v0           # add gid*4 to cBuffer pointer
-        v_addc_u32 v3, vcc, v3, v1, vcc
+        VADD_U32 v2, vcc, s6, v0           # add gid*4 to cBuffer pointer
+        VADDC_U32 v3, vcc, v3, v1, vcc
         flat_store_dword v[2:3], v6
     .endif
 .endif
