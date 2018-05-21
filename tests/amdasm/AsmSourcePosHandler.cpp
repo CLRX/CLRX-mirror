bdf195b9fa2476821ccf7c389994a2ea816cef1b
matszpk <matszpk@interia.pl>
2018-05-21 06:49:21 +0000

    CLRadeonExtender: Asm: Introduce uniqueId to AsmSource and AsmMacroSubst to correct comparing. Use correct comparison of macroSubsts and sources in AsmSourcePosHandler. Add unfinished AsmSourcePosHandler test. Add get to RegPtr.

---
+++ tests/amdasm/AsmSourcePosHandler.cpp
@@ -0,0 +1,49 @@
+/*
+ *  CLRadeonExtender - Unofficial OpenCL Radeon Extensions Library
+ *  Copyright (C) 2014-2018 Mateusz Szpakowski
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <CLRX/Config.h>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <CLRX/utils/Containers.h>
+#include <CLRX/amdasm/AsmSource.h>
+#include "../TestUtils.h"
+
+using namespace CLRX;
+
+struct SourcePosEntry
+{
+    size_t offset;
+    const char* macroName;
+    const char* filename;
+    LineNo lineNo;
+    ColNo colNo;
+};
+
+typedef Array<SourcePosEntry> SourcePosHandlerTestCase;
+
+static void testAsmSourcePosHandler(cxuint i, const SourcePosHandlerTestCase& testCase)
+{
+    AsmSourcePosHandler sourcePosHandler;
+}
+
+int main(int argc, const char** argv)
+{
+    return 0;
+}
